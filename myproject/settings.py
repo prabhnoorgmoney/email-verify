828cfe238bfa96a80598f2b8b56c20e843e0d5a3
prabhnoorgmoney <135623540+prabhnoorgmoney@users.noreply.github.com>
2024-02-07 13:43:00 +0530

    Update settings.py

--- myproject/settings.py
+++ myproject/settings.py
@@ -23,8 +23,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-fmb-z!tfrgem+-qc_2-4bfba8iifasy9j6(g@(%fgl53(!4vn-"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = True
-DEBUG = False
+ DEBUG = True
+
 
 # ALLOWED_HOSTS = []
 ALLOWED_HOSTS = ['*']
